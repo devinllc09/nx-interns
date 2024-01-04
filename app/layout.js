bd3ceac49db56fe607cc49c50c18837ec5a931b7
devinllc09 <incrypto09@gmail.com>
2024-01-04 03:27:35 -0800

    rv

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'NextJS Assignment',
+  title: 'Next Ramesh Assignment',
   description: 'Create by devinllc09',
 }
 
